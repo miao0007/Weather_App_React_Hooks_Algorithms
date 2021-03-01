d51c2986e88cfd352a0357d2d420f29c73a47aad
miao0007 <miao188@hotmail.com>
2021-03-01 21:54:58 +1030

    fetchData through third party API

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Form from './Form';
+import Weather from './Weather';
+
 
 function App() {
+  const [weather,setWeather] = useState([])
+  const API_KEY = 'ee7c3298c2fab93deaad71567a82a537';
+
+  async function fetchData(e){
+    e.preventDefault();
+    const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`)
+    .then(res =>res.json())
+    .then(data =>data)
+    setWeather({
+      data: apiData
+    })
+
+  }
   return (
     <div className="App">
       <h3>WEATHER APP</h3>
+      <Form getWeather={fetchData} />
+      {console.log(weather.data)}
     </div>
   );
 }
