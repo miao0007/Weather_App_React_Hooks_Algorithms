2c64339f36756543115b30375edf28c9a554f02c
miao0007 <miao188@hotmail.com>
2021-03-01 22:32:25 +1030

    keywords match

--- weather-react/src/Weather.js
+++ weather-react/src/Weather.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const Weather = ({description, city, country, error, temperature}) => {
+    if (description) {
+        const weatherDescription = description.split(' ')
+        const keyWords = ['cloudy','clouds','cloud','overcast']
+        for (let i = 0; i <weatherDescription.length; i++){
+            if(keyWords.includes(weatherDescription[i])) {
+                console.log(weatherDescription[i], ': we have match')
+            }
+        }
+        console.log(keyWords);
+        console.log(weatherDescription);
+    }
     return (
         // writing the name of our data followed by && is to ensure that the API has been passed on and true before proceeding to return the data
         <div>
