3cac1cae6aa61a612e71605e17c5283d2ebc6b76
miao0007 <miao188@hotmail.com>
2021-03-01 22:20:04 +1030

    specifying attributes to weather description

--- weather-react/src/Weather.js
+++ weather-react/src/Weather.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Weather = ({description, city, country, error, temperature}) => {
     return (
+        // writing the name of our data followed by && is to ensure that the API has been passed on and true before proceeding to return the data
         <div>
+            
             {city && country && <p>{city}, {country}</p>}
-            {temperature && <p>{temperature}</p>}
+            {temperature && <p>{temperature} °F</p>}
             {description && <p>{description}</p>}
             {error && <p>{error}</p>}
         </div>
