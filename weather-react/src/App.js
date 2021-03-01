04135cf8c29154b2bd10ad5d43142ee792202f2e
miao0007 <miao188@hotmail.com>
2021-03-01 22:06:57 +1030

    dynamic weather state

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -9,12 +9,20 @@ function App() {
   const API_KEY = 'ee7c3298c2fab93deaad71567a82a537';
 
   async function fetchData(e){
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
     e.preventDefault();
-    const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`)
+    const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
+    ${city},${country}&APPID=${API_KEY}`)
     .then(res =>res.json())
     .then(data =>data)
     setWeather({
-      data: apiData
+      data: apiData,
+      city:apiData.city,
+      country: apiData.country,
+      description:apiData.weather[0].description,
+      temperature:apiData.main.temp,
+      error:""
     })
 
   }
@@ -22,6 +30,13 @@ function App() {
     <div className="App">
       <h3>WEATHER APP</h3>
       <Form getWeather={fetchData} />
+      <Weather
+      city = {weather.city}
+      country = {weather.country}
+      description = {weather.description}
+      temperature = {weather.temperature}
+      error = {weather.error}
+      />
       {console.log(weather.data)}
     </div>
   );
